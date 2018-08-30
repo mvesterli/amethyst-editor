5615303582e4dff290b1216824264dd9d1ac8630
David LeGare <excaliburhissheath@gmail.com>
2018-08-30 01:50:31 -0500

    Display all components attached to an entity

--- renderer.js
+++ renderer.js
@@ -1,11 +1,12 @@
 const { ipcRenderer } = require('electron');
 
-exports.app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
         process: process,
         entities: [],
-        transforms: {},
+        components: [],
+        rawComponents: null,
         selectedEntity: null,
     },
     methods: {
@@ -15,13 +16,26 @@ exports.app = new Vue({
         }
     }
 });
+exports.app = app;
 
 ipcRenderer.on('message', (event, data) => {
-    exports.app.entities = data.entities;
+    app.entities = data.entities;
+    app.rawComponents = data.components;
 
-    let transforms = {};
-    for (let [entityId, transform] of data.transforms) {
-        transforms['' + entityId] = transform;
+    // Convert the list of [key, value] pairs into maps that we can use to
+    // quickly lookup which components are associated with which entities.
+    //
+    // TODO: Could we have Rust send the data directly in the format we want?
+    let components = [];
+    for (let [type, flatData] of Object.entries(data.components)) {
+        let lookup = {};
+        for (let [entity, componentData] of flatData) {
+            lookup[entity] = componentData;
+        }
+        components.push({
+            type: type,
+            data: lookup,
+        });
     }
-    exports.app.transforms = transforms;
+    app.components = components;
 });
