8419e7a301b259b43a0fe84890fb682875468fec
David LeGare <excaliburhissheath@gmail.com>
2018-08-29 22:17:59 -0500

    Send transform data to the editor

--- ipc-test/src/systems/sync_editor.rs
+++ ipc-test/src/systems/sync_editor.rs
@@ -1,4 +1,5 @@
 use {EntityData, Message};
+use amethyst::core::Transform;
 use amethyst::ecs::*;
 use serde_json;
 use std::net::UdpSocket;
@@ -6,30 +7,42 @@ use std::net::UdpSocket;
 #[derive(Debug, Clone, Copy, Default)]
 pub struct SyncEditorSystem;
 
+#[derive(Debug, Serialize)]
+struct Data<'a> {
+    entities: Vec<EntityData>,
+    transforms: Vec<(u32, &'a Transform)>,
+}
+
 impl<'a> System<'a> for SyncEditorSystem {
     type SystemData = (
         ReadExpect<'a, UdpSocket>,
         Entities<'a>,
+        ReadStorage<'a, Transform>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (socket, entities) = data;
+        let (socket, entities, transforms) = data;
 
-        let entity_data = {
-            let mut result = Vec::new();
-            for (entity,) in (&*entities,).join() {
-                result.push(EntityData {
-                    id: entity.id(),
-                    generation: entity.gen().id(),
-                });
-            }
-            result
-        };
+        let mut entity_data = Vec::new();
+        for (entity,) in (&*entities,).join() {
+            entity_data.push(EntityData {
+                id: entity.id(),
+                generation: entity.gen().id(),
+            });
+        }
+
+        let mut transform_data = Vec::new();
+        for (entity, transform) in (&*entities, &transforms).join() {
+            transform_data.push((entity.id(), transform));
+        }
 
         // Create the message and serialize it to JSON.
         let message = Message {
             ty: "message",
-            data: entity_data,
+            data: Data {
+                entities: entity_data,
+                transforms: transform_data,
+            },
         };
         let mut message_string = serde_json::to_string(&message).expect("Failed to serialize message");
 
