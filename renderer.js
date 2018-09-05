432496544e3d0e2ce1784ce6c6f5eca79068accf
David LeGare <excaliburhissheath@gmail.com>
2018-09-05 15:34:19 -0500

    Ensure consistent ordering for components/resources

--- renderer.js
+++ renderer.js
@@ -20,6 +20,22 @@ exports.app = app;
 
 ipcRenderer.on('message', (event, data) => {
     app.entities = data.entities;
-    app.components = data.components;
-    app.resources = data.resources;
+
+    // Sort components before updating the local data to ensure that components always appear
+    // in the same order regardless of the order they are sent in.
+    var sortedComponents = data.components;
+    sortedComponents.sort(compareNamed);
+    app.components = sortedComponents;
+
+    // Sort resources before updating the local data to ensure that resources always appear
+    // in the same order regardless of the order they are sent in.
+    var sortedResources = data.resources;
+    sortedResources.sort(compareNamed);
+    app.resources = sortedResources;
 });
+
+function compareNamed(left, right) {
+    if (left.name < right.name) { return -1; }
+    if (left.name > right.name) { return 1; }
+    return 0;
+}
